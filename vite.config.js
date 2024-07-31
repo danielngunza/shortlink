f05946234160e454b9c41ac1512c4bb61ee0468f
Daniel César Ngunza <danielcesarngunza00@gmail.com>
2024-08-01 00:15:00 +0100

    Descrição das mudanças feitas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/shortlink"
-})
+  plugins: [react(), ghPages()],
+  base: '/NOME_DO_REPOSITORIO/',  // Substitua pelo nome do seu repositório
+});
