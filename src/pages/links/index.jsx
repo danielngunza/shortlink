43185d96ba60e6f17fbb8df4ac54461e39ddc94b
Daniel César Ngunza <danielcesarngunza00@gmail.com>
2024-07-31 19:12:51 +0100

    atualuzando as funcionalidades

--- src/pages/links/index.jsx
+++ src/pages/links/index.jsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react'
 import './links.css'
 import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
+import { getLinkSave } from '../../servicos/storelink'
 
 function Links() {
+
+  const [myLinks, setMylinks] = useState([])
+  const [data, setData] = useState({})
+  const [showModa, setShowModal] = useState(false)
+  
+  useEffect(() => {
+    async function getLinks(){
+
+      const result = await getLinkSave('@encurtalink')
+      if(result.length === 0){
+        console.log("LISTA VASZIA")
+      }
+      setMylinks(result)
+    }
+    getLinks()
+  }, [])
+
+
+
 
   return (
     <div className='links-container'>
@@ -13,26 +34,19 @@ function Links() {
         
           <h1>Meus Links </h1>
       </div>
-      <div className='links-item'>
+      { myLinks.map( link => (
+         <div key={link.id} className='links-item'>
         <button className='link'>
           <FiLink size={18} color='#fff' />
-          https://danielngunza.com
+          {link.long_url}
         </button>
         <button className='link-delete'>
           <FiTrash size={24} color='#ff5454'/>
         </button>
 
       </div>
-      <div className='links-item'>
-        <button className='link'>
-          <FiLink size={18} color='#fff' />
-          https://danielngunza.com
-        </button>
-        <button className='link-delete'>
-          <FiTrash size={24} color='#ff5454'/>
-        </button>
-
-      </div>
+      ) )}
+      
     </div>
   )
 }
