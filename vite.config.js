9497c8c67fc77c1bb28629b26aa4ca3b5aed38f7
Daniel César Ngunza <danielcesarngunza00@gmail.com>
2024-08-01 01:18:04 +0100

    bug resolvido, eu acho kkkk

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages';
-
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: "/shortlink"  // Substitua pelo nome do seu repositório
+  plugins: [react()],
+  base: "/",  // Substitua pelo nome do seu repositório
 });
+
+
